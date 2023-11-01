21e8945493dc9f1f5502424d0f25a4611c3d93fb
Ese Amadasun <amadasunese@gmail.com>
2023-11-01 19:55:09 +0100

    updated

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,13 +1,4 @@
 #!/usr/bin/env python3
-"""Regex-ing"""
-
-import re
-
-
-def filter_datum(fields, redaction, message, separator):
-    return re.sub('|'.join([f'(?<={f}{separator})[^{separator}]*'
-                            for f in fields]), redaction, message)
-#!/usr/bin/env python3
 """
 Module for handling Personal Data
 """
