212cbd6394993700c566a9e7fbcca8e7a108b878
Ese Amadasun <amadasunese@gmail.com>
2023-11-01 19:44:57 +0100

    updated

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """Regex-ing"""
+
 import re
 
 
 def filter_datum(fields, redaction, message, separator):
-    return re.sub('|'.join([f'(?<={f}{separator})[^{separator}]*']) for f in fields, redaction, message)
-
+    return re.sub('|'.join([f'(?<={f}{separator})[^{separator}]*'
+                            for f in fields]), redaction, message)
